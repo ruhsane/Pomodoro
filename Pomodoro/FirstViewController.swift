7cbaf4a7facc7cee1f47b3b1f0bcf4e65b53b0b2
Rushani Shawuti <ruhsane924@gmail.com>
2019-02-05 12:31:23 -0800

    countdown works

---
+++ Pomodoro/FirstViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FirstViewController.swift
+//  Pomodoro
+//
+//  Created by Adriana González Martínez on 1/16/19.
+//  Copyright © 2019 Adriana González Martínez. All rights reserved.
+//  Initial app found here www.globalnerdy.com/2017/06/28/the-code-for-tampa-ios-meetups-pomodoro-timer-exercise
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    var completedCycles = 0
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    deinit {
+        //ACTION: Remove observers
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: "Notf"), object: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //ACTION: Add observers
+        NotificationCenter.default.addObserver(self, selector: #selector(receivedNotification(_:)), name: NSNotification.Name(rawValue: "Notf"), object: nil)
+        
+    }
+    
+    @objc func receivedNotification(_ notification:Notification) {
+        // ACTION: Update value of completed cycles
+        // ACTION: Update message label
+        completedCycles += 1
+        
+    }
+}
+
